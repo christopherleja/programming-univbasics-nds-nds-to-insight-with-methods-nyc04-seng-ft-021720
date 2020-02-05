b7b5bd533dc3ea53a1a5067206c84f426340a4b7
Christopher Leja <christopher.leja@gmail.com>
2020-02-05 16:01:07 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -29,9 +29,9 @@ def directors_totals(nds)
     puts director_name[row_index]
     #puts gross_for_director(director_data)
     gross_for_director(director_data)
+    result["#{director_name[row_index]}"] = gross_for_director(director_data)
     row_index += 1
   end
-    result["#{director_name[row_index]}"] = gross_for_director(director_data)
   result
   puts result
 end
